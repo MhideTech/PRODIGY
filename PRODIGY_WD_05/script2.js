74cdf6b3a79824d44ff74ab1cb91b1be514098d1
MhideTech <okefolahanolamide2006@gmail.com>
2024-05-09 17:00:48 +0100

    Finished weather application

--- PRODIGY_WD_05/script2.js
+++ PRODIGY_WD_05/script2.js
@@ -3,29 +3,29 @@ const locationForm = document.querySelector("#location-form");
 const locationInput = document.querySelector("#location-input");
 const submitBtn = document.querySelector("#submit-btn");
 
-submitBtn.addEventListener('click', function(e){
-    e.preventDefault()
+submitBtn.addEventListener("click", function (e) {
+  e.preventDefault();
 
-    const query = locationInput.value;    
-    
-    const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${query}`;
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '16b5b415b1msh7cfdea873864269p13a51bjsn5ec40d5e83c6',
-            'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-        }
-    };
+  const query = locationInput.value;
 
-    async function getWeatherData() {
-        try {
-            weatherInfo.innerHTML = ``
-            const response = await fetch(url, options);
-            const data = await response.json();
-            console.log(data);
+  const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${query}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "16b5b415b1msh7cfdea873864269p13a51bjsn5ec40d5e83c6",
+      "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+    },
+  };
 
-            data
-              ? (weatherInfo.innerHTML = `
+  async function getWeatherData() {
+    try {
+      weatherInfo.innerHTML = ``;
+      const response = await fetch(url, options);
+      const data = await response.json();
+      console.log(data);
+
+      !data.error
+        ? (weatherInfo.innerHTML = `
                 <img src=${data.current.condition.icon} alt='' />
                 <h2>${data.current.temp_c}<sup>o</sup>C</h2>
                 <h3>${data.current.condition.text}</h3> 
@@ -48,16 +48,14 @@ submitBtn.addEventListener('click', function(e){
                     </div>
                 </div>
             `)
-              : (weatherInfo.innerHTML = `
-                <p>${data.error.message}</p>
+        : (weatherInfo.innerHTML = `
+                <p style="margin-top: 50px;">${data.error.message}</p>
             `);
-            console.log(data.error.message);
-        } catch (error) {
-            
-            console.error(error);
-        }
-
+      console.log(data.error.message);
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    getWeatherData();    
-})
+  getWeatherData();
+});
